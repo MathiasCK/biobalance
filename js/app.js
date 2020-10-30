4c9125ba3284d6b792d8f8f8598b70c1ce66da2b
Sanna Jammeh <sannajam@outlook.com>
2020-10-30 17:02:23 +0100

    Space add gallery

--- js/app.js
+++ js/app.js
@@ -3,27 +3,31 @@
 const sections = document.querySelectorAll('section');
 const navBar = document.querySelector('.nav-bar');
 const navItems = document.querySelectorAll('.nav-item');
-navBar.classList.remove("white")
+navBar.classList.remove('white');
 
-window.addEventListener('scroll',() => {
-    // Change navbar color
-    const currentIndex = sections.length - [...sections].reverse().findIndex((section) => window.scrollY >= section.offsetTop - 57) - 1;
-    if (currentIndex === sections.lengh) return;
+window.addEventListener('scroll', () => {
+	// Change navbar color
+	const currentIndex =
+		sections.length -
+		[...sections]
+			.reverse()
+			.findIndex((section) => window.scrollY >= section.offsetTop - 57) -
+		1;
+	if (currentIndex === sections.lengh) return;
 
-    const currentSection = sections[currentIndex];
-    navBar.className = `nav-bar ${currentSection.dataset.background}`;
+	const currentSection = sections[currentIndex];
+	navBar.className = `nav-bar ${currentSection.dataset.background}`;
 
-    // Add class active to nav links 
-    if (currentIndex === sections.length) return (document.title = `Treecubator | Saving the rainforest`);
+	// Add class active to nav links
+	if (currentIndex === sections.length)
+		return (document.title = `Treecubator | Saving the rainforest`);
 
-    navItems.forEach((navItem) => {
-        navItem.classList.remove('active');
-    });
-    const currentItem = navItems[currentIndex];
-    if (!currentItem.classList.contains('active')) currentItem.classList.add('active');
-    const title = `Treecubator | ${currentItem.innerHTML}`;
-    if (document.title !== title) document.title = title;
-
-    
-})
-
+	navItems.forEach((navItem) => {
+		navItem.classList.remove('active');
+	});
+	const currentItem = navItems[currentIndex];
+	if (!currentItem.classList.contains('active'))
+		currentItem.classList.add('active');
+	const title = `Treecubator | ${currentItem.innerHTML}`;
+	if (document.title !== title) document.title = title;
+});
