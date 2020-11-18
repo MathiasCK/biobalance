34734525d9d28ab6f76cb17908bd01f61cda9a00
Sanna Jammeh <sannajam@outlook.com>
2020-11-18 18:38:47 +0100

    Turn off gsap in dev

--- js/app.js
+++ js/app.js
@@ -101,11 +101,11 @@ const introHTML = /*html*/ `
     </div>
 </div>
 `;
-introContainer.innerHTML = introHTML;
+// introContainer.innerHTML = introHTML;
 
-tl.set('.content', { css: { opacity: 0 } });
-tl.set('.nav-bar', { css: { opacity: 0 } });
+// tl.set('.content', { css: { opacity: 0 } });
+// tl.set('.nav-bar', { css: { opacity: 0 } });
 
-tl.to('.text', { y: '0%', duration: 1, stagger: 0.33 });
-tl.to('.intro', { y: '-100%', duration: 1, delay: 1 }, '-=1');
-tl.to(['.content', '.nav-bar'], { opacity: 1, duration: 1 }, '-=0.5');
+// tl.to('.text', { y: '0%', duration: 1, stagger: 0.33 });
+// tl.to('.intro', { y: '-100%', duration: 1, delay: 1 }, '-=1');
+// tl.to(['.content', '.nav-bar'], { opacity: 1, duration: 1 }, '-=0.5');
