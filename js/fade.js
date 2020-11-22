db56e0c2335c16f6dc522fd9e79926fd5bea0c04
Mathias Kolberg <Mathias.ck2000@gmail.com>
2020-11-22 20:29:15 +0100

    animations and span

--- js/fade.js
+++ js/fade.js
@@ -1,17 +1,32 @@
-// Make elemenst fade in and out
+// This concept is ispired by make-it-responsive 
+let scrollingUp;
+let oldScroll = 0;
+
+window.addEventListener('scroll', () => {
+    const newScroll = oldScroll > window.scrollY;
+    if(scrollingUp !== newScroll) {
+        scrollingUp = newScroll;
+    }
+    oldScroll = window.scrollY;
+})
+
 const observerOptions = {
     root: null,
     rootMargin: '0px',
     threshold: 0.7,
   };
   
-  function observerCallback(entries, observer) {
+  function observerCallback(entries) {
     entries.forEach((entry) => {
+        entry.target.classList.remove('up');
       if (entry.isIntersecting) {
         entry.target.classList.replace('fadeOut', 'fadeIn');
       } else {
         if (entry.target.classList.contains('fadeIn')) {
             entry.target.classList.replace('fadeIn', 'fadeOut');
+            if (!scrollingUp) {
+                entry.target.classList.add('up');
+            }
         }
       }
     });
